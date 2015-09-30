1139442354021e8fea536b046668555a45b1fdbe
Blake Lieberman <blake.lieberman@gmail.com>
2015-09-30 09:41:21 -0700

    update the mobile view

--- app/scripts/filter.js
+++ app/scripts/filter.js
@@ -57,8 +57,9 @@ jQuery(document).ready(function($){
 
 	function fixGallery() {
 		var offsetTop = $('.cd-main-content').offset().top,
-			scrollTop = $(window).scrollTop();
-		( scrollTop >= offsetTop ) ? $('.cd-main-content').addClass('is-fixed') : $('.cd-main-content').removeClass('is-fixed');
+			scrollTop = $(window).scrollTop(),
+			headerHeight = $('.main-header').height();
+		( scrollTop + headerHeight >= offsetTop ) ? $('.cd-main-content').addClass('is-fixed') : $('.cd-main-content').removeClass('is-fixed');
 	}
 
 	/************************************
